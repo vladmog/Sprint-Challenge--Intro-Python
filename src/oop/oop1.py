bbaea2d0802bc628f4f431f02d3d6d8886083666
vladmog <vladmog@gmail.com>
2019-11-01 12:27:06 -0700

    All tests passing, odd formatting issue on cityreader

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -23,3 +23,18 @@ class Vehicle: # base class
 
 class FlightVehicle(Vehicle):
     pass
+
+class Starship(FlightVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
